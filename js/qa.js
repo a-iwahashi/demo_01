7fb7978008a041e8192d728a8e9694b10448df76
iwhsak <iwahashi.unt@gmail.com>
2020-02-25 10:47:28 +0900

    [update] .main-faq click motion

--- js/qa.js
+++ js/qa.js
@@ -1,6 +1,7 @@
 $(function(){
     $('.main-faq dd').hide();
     $('.main-faq dt').click(function(){
-        $('+dd',this).slideToggle(this);
+        $(this).not(':animated').toggleClass('main-faq__que--change');
+        $('+dd',this).not(':animated').slideToggle(this);
     });
 });
